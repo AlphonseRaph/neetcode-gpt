6c9550276cbb1fcc867f425334eb51b06d25fe03
Alphonse Oketch <alphonseoketch2004@gmail.com>
2026-05-11 12:18:53 +0300

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -10,19 +10,34 @@ class Solution:
         # b: scalar bias
         # y_true: true target value
         #
-    
-        
+        # -----------------------------
+        # FORWARD PASS
+        # -----------------------------
+        # Linear combination
         z = (np.dot(x, w) + b )
+        # Sigmoid activation
         y_hat = 1 /( 1 + np.exp(-z))
-        loss = y_hat - y_true
-        # derivative of sigmoid
-        sig_deriv = y_hat * (1 - y_hat)
-        # delta
-        delta = sig_deriv * loss
-        # blaming specific weights (dL/dw)
-        dL_dw = delta * x
+        # Actual Mean Squared Error loss
+        mse_loss = 0.5 * (y_hat - y_true) ** 2
+
+        # -----------------------------
+        # BACKWARD PASS
+        # -----------------------------
+
+        # dL/dy_hat
+        dL_dyhat = y_hat - y_true
+
+        #  dy_hat/dz  (derivative of sigmoid)
+        dyhat_dz = y_hat * (1 - y_hat)
+        # dL/dz  (chain rule) / delta
+        dL_dz = dyhat_dz * dL_dyhat
+        # blaming specific weights 
+        # dL/dw
+        dL_dw = dL_dz * x
         # blaming bias (dL/db)
-        dL_db = delta  
+        dL_db = dL_dz 
+
+
         # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
         # Loss: L = 0.5 * (y_hat - y_true)^2
         # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
